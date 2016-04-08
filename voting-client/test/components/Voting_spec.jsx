3eeba7f416fdfcb784b21a97e00f96b62e5f3672
kusera <kuserasera@gmail.com>
2016-04-08 16:41:48 -0400

    Add tests for winner and vote rendering

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -22,8 +22,8 @@ describe('Voting', () => {
   });
 
   it('invokeds callback when a button is clicked', () => {
-    let votedWidth;
-    const vote = (entry) => votedWidth = entry;
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
 
     const component = renderIntoDocument(
       <Voting pair={["Trainspotting", "28 Days Later"]}
@@ -32,6 +32,42 @@ describe('Voting', () => {
     const buttons = scryRenderedDOMComponentsWithClass(component, 'button');
     Simulate.click(buttons[0]);
 
-    expect(votedWidth).to.equal('Trainspotting');
+    expect(votedWith).to.equal('Trainspotting');
+  });
+
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting"/>
+    );
+
+    const buttons = scryRenderedDOMComponentsWithClass(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+
+  it('adds label to the voted entry', () => {
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+              hasVoted="Trainspotting"/>
+    );
+
+    const buttons = scryRenderedDOMComponentsWithClass(component, 'button');
+    expect(buttons[0].childNodes[1].textContent).to.contain('Voted');
+  });
+
+  it('renders just the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Voting winner="Trainspotting"/>
+    );
+
+    const buttons = scryRenderedDOMComponentsWithClass(component, 'button');
+    expect(buttons.length).to.equal(0);
+
+    const winner = ReactDOM.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
   })
 });
