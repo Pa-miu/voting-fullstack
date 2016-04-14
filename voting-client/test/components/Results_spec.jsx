f68190d02a3ffb76676111e0d696a7e57463c41c
kusera <kuserasera@gmail.com>
2016-04-14 17:38:23 -0400

    Add results component and routing

---
+++ voting-client/test/components/Results_spec.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithClass
+} from 'react-addons-test-utils';
+import {List} from 'immutable';
+import Results from '../../src/components/Results';
+import {expect} from 'chai';
+
+describe ('Results', () => {
+  
+})
